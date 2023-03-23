07c911c102ac8b904269af86d2b90e373ae688a0
JamoDevy <devinlclarke@gmail.com>
2023-03-23 02:07:01 -0400

    submitting assignment

--- src/script.js
+++ src/script.js
@@ -4,6 +4,7 @@ class MovieFinder extends React.Component {
       this.state = {
         searchTerm: '',
         results: [],
+        error: '',
       };
   
       this.handleChange = this.handleChange.bind(this);
@@ -15,35 +16,89 @@ class MovieFinder extends React.Component {
     }
   
     handleSubmit(event) {
-      event.preventDefault();
-      // doing nothing for now
-    }
+        event.preventDefault();
+        let { searchTerm } = this.state;  // ES6 destructuring
+        searchTerm = searchTerm.trim();  // clean the string
+        if (!searchTerm) {  // make sure the value isn't an empty string
+          return;  // early return
+        }
+      
+        // make the AJAX request to OMDBAPI to get a list of results
+        fetch(`https://www.omdbapi.com/?s=${searchTerm}&apikey=b7da8d63`)
+            .then(checkStatus)
+            .then(json)
+            .then(data => {
+        if (data.Response === 'False') {
+            throw new Error(data.Error);
+            }
+        if (data.Response === 'True' && data.Search) {
+            this.setState({ results: data.Search, error: '' });
+        }
+  })
+  .catch(error => {
+    this.setState({ error: error.message });
+    console.log(error);
+  })
+      }
   
-    render() {
-      const { searchTerm, results } = this.state;  // ES6 destructuring
-  
-      return (
-        <div className="container">
-          <div className="row">
-            <div className="col-12">
-              <form onSubmit={this.handleSubmit} className="form-inline my-4">
-                <input
-                  type="text"
-                  className="form-control mr-sm-2"
-                  placeholder="frozen"
-                  value={searchTerm}
-                  onChange={this.handleChange}
-                />
-                <button type="submit" className="btn btn-primary">Submit</button>
-              </form>
-              {results.map((movie) => {
-                return null;  // returns nothing for now
-              })}
+      render() {
+        const { searchTerm, results, error } = this.state;
+        return (
+          <div className="container">
+            <div className="row">
+              <div className="col-12">
+                <form onSubmit={this.handleSubmit} className="form-inline my-4">
+                  <input
+                    type="text"
+                    className="form-control mr-sm-2"
+                    placeholder="frozen"
+                    value={searchTerm}
+                    onChange={this.handleChange}
+                  />
+                  <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
+                {(() => {
+                  if (error) {
+                    return error;
+                  }
+                  return results.map((movie) => {
+                    return <Movie key={movie.imdbID} movie={movie} />;
+                  })
+                })()}
+              </div>
             </div>
           </div>
-        </div>
-      )
+        )
+      }
+  }
+
+  const checkStatus = (response) => {
+    if (response.ok) {
+      // .ok returns true if response status is 200-299
+      return response;
     }
+    throw new Error('Request was either a 404 or 500');
+  }
+  const json = (response) => response.json()
+
+  const Movie = (props) => {
+    const { Title, Year, imdbID, Type, Poster } = props.movie;  // ES6 destructuring
+  
+    return (
+      <div className="row">
+        <div className="col-4 col-md-3 mb-3">
+          <a href={`https://www.imdb.com/title/${imdbID}/`} target="_blank">
+            <img src={Poster} className="img-fluid" />
+          </a>
+        </div>
+        <div className="col-8 col-md-9 mb-3">
+          <a href={`https://www.imdb.com/title/${imdbID}/`} target="_blank">
+            <h4>{Title}</h4>
+            <p>{Type} | {Year}</p>
+          </a>
+        </div>
+      </div>
+    )
   }
   
   ReactDOM.render(
